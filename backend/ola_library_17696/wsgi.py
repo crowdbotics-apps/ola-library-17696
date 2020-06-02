98d6abba76f5be29085d02e5f651dc703c6047b3
Crowdbotics <team@crowdbotics.com>
2020-06-02 10:51:48 +0000

    Update models

--- backend/ola_library_17696/wsgi.py
+++ backend/ola_library_17696/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ola_library_17696.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ola_library_17696.settings")
 
 application = get_wsgi_application()
